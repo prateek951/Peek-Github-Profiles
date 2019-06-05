f205c515fb2666ae6779ec3d4a72f904bdbfab30
prateek951 <prince.cenation@gmail.com>
2019-06-05 10:52:36 +0530

    converted appnavbar and useritem to functional components

--- src/components/layouts/AppNavbar.js
+++ src/components/layouts/AppNavbar.js
@@ -1,39 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class AppNavbar extends Component {
-  //defaultProps
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fas fa-github"
-  };
-  // propTypes
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
+const AppNavbar = ({ title, icon }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} />
+        <span /> {title}
+      </h1>
+    </nav>
+  );
+};
 
-  render() {
-    const { title, icon } = this.props;
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={icon} />
-          <span /> {title}
-        </h1>
-      </nav>
-    );
-  }
-}
-
-// Setup the default props if the component does not receive any props
-// it will make use of the default props
-
-// AppNavbar.defaultProps = {
-//   title: "GithubProfile",
-//   icon: "fab fa-github"
-// };
-
+//defaultProps
+AppNavbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fas fa-github"
+};
+// propTypes
 AppNavbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired
