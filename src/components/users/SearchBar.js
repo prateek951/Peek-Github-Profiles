82851a76ad8c602b6a41df0cfa182c137551e13b
prateek951 <prince.cenation@gmail.com>
2019-06-27 13:36:57 +0530

    refactored searchbar to functional component to use state

--- src/components/users/SearchBar.js
+++ src/components/users/SearchBar.js
@@ -1,67 +1,50 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      term: ""
-    };
-    this.bindEvents();
-  }
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    setAlert: PropTypes.func.isRequired,
-    show: PropTypes.bool.isRequired,
-    clearUsers: PropTypes.func.isRequired
+const SearchBar = ({ searchUsers, setAlert, show, clearUsers }) => {
+  const [term, setTerm] = useState("");
+  const handleStringChange = event => {
+    setTerm(event.target.value);
   };
-
-  bindEvents() {
-    this.handleStringChange = this.handleStringChange.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-  }
-  handleStringChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-  handleSearch(e) {
-    e.preventDefault();
-    const { term } = this.state;
-    if (term === "" ||  term.trim().length === 0) {
-      this.props.setAlert("Please specify a github handle", "danger");
+  const handleSearch = event => {
+    event.preventDefault();
+    if (term === "" || term.trim().length === 0) {
+      setAlert("Please specify a github handle", "danger");
     } else {
-      this.props.searchUsers(term);
-      this.setState({ term: "" });
+      searchUsers(term);
+      setTerm("");
     }
-  }
-  render() {
-    const {
-      state: { term },
-      props: { show, clearUsers }
-    } = this;
-    return (
-      <div>
-        <form onSubmit={this.handleSearch}>
-          <input
-            type="text"
-            placeholder="Enter a username to filter by"
-            name="term"
-            value={term}
-            onChange={this.handleStringChange}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {show && (
-          <button className="btn btn-light btn-block" onClick={clearUsers}>
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+  };
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Enter a username to filter by"
+          name="term"
+          value={term}
+          onChange={handleStringChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {show && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+SearchBar.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  setAlert: PropTypes.func.isRequired,
+  show: PropTypes.bool.isRequired,
+  clearUsers: PropTypes.func.isRequired
+};
+
 export default SearchBar;
